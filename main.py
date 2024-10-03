60686bed1f9273cb8a032335264d75f780d03e57
vlado252 <vladgremlin1337@gmail.com>
2024-10-03 10:08:53 +0300

    Задание 3

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-n = int(input("Введите количество школьников:"))
-k = int(input("Введите количество яблок: "))
+day = int(input("Введи день:"))
+month = int(input("Введи номер месяца:"))
 
-yabloki = k // n
-ostatok = k - yabloki*n
-
-
-print(yabloki)
-print ("По ", yabloki, "каждому школьнику.")
-print(ostatok, "яблок останется в корзине.")
+if (day>=1 and day <=31 and month==12)or(day>=1 and day <=31 and month==1)or(day>=1 and day <=28 and month==2):
+   print("Сезон зима") 
+elif(day>=1 and day <=31 and month==3)or(day>=1 and day <=30 and month==4)or(day>=1 and day <=31 and month==5):
+   print("Сезон весна") 
+elif(day>=1 and day <=30 and month==6)or(day>=1 and day <=31 and month==7)or(day>=1 and day <=31 and month==8) :
+   print("Сезон лето") 
+elif(day>=1 and day <=30 and month==9)or(day>=1 and day <=31 and month==10)or(day>=1 and day <=30 and month==11) :
+   print("Сезон осень") 
+else:
+   print("Неправильная дата")
